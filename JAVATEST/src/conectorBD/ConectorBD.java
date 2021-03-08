3d7cedd3fc6f8e9ec4ac8ba664af4122376c9439
diegor06 <65474383+diegor06@users.noreply.github.com>
2021-03-08 14:58:33 -0500

    Base de datos, conexion.

--- JAVATEST/src/conectorBD/ConectorBD.java
+++ JAVATEST/src/conectorBD/ConectorBD.java
@@ -1,5 +1,26 @@
 package conectorBD;
 
-public class ConectorBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConectorBD {
+	public static final String URL = "jdbc:mysql://localhost:3306/Proyecto";
+	public static final String USER = "root";
+	public static final String PASSWORD = "1234";
+
+	public static Connection establecerConexion() {
+		Connection c = null;
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			c = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Conexion exitosa con la base de datos.");
+		} catch (Exception e) {
+			System.out.println("Conexion no exitosa con la base de datos, verifique. error: " + e.getMessage());
+		}
+
+		return c;
+
+	}
 }
